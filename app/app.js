58fe003ec04967269da650744500b8439c42a74c
Genevieve Parkes <genevieve.parkes@gmail.com>
2015-11-30 14:06:21 +0100

    remove seed app add bootstrap and index view html

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+var myApp = angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
+
+myApp.controller('mainController', function ($scope, $http) {
+  $scope.formData = {};
+  $scope.repos = {};
+});
